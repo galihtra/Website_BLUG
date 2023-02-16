59a90919245b68392cf5561d57b62416e47f4660
Galih Tri Risky Andiko <galihtririskyandiko@gmail.com>
2023-02-16 20:25:13 +0700

    initial commit

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,217 @@
+@extends('layouts.main')
+
+@section('content')
+<h1 class="h3 mb-2 text-gray-800">Admin</h1>
+
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+  <div class="card-body">
+    <button
+      type="button"
+      class="btn btn-primary mb-4"
+      data-toggle="modal"
+      data-target="#exampleModal"
+    >
+      Tambah Admin
+    </button>
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">
+              Tambah Admin
+            </h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group">
+                <label for="nama-admin" class="col-form-label"
+                  >Nama</label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  id="nama-admin"
+                />
+              </div>
+              <div class="form-group">
+                <label for="username-admin" class="col-form-label"
+                  >Username</label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  id="username-admin"
+                />
+              </div>
+              <div class="form-group">
+                <label for="password-admin" class="col-form-label"
+                  >Password</label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  id="password-admin"
+                />
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">
+              Tambah
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="table-responsive">
+      <table
+        class="table"
+        id="dataTable"
+        width="100%"
+        cellspacing="0"
+      >
+        <thead>
+          <tr>
+            <th>Nama</th>
+            <th>Username</th>
+            <th>Status</th>
+            <th>Aksi</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Inti</td>
+            <td>Inti Blug</td>
+            <td class="text-success">Aktif</td>
+            <td>
+              <button type="button" class="btn btn-info">
+                <i class="fas fa-lock"></i>
+              </button>
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-toggle="modal"
+                data-target="#ubah-admin"
+              >
+                <i class="fas fa-pen"></i>
+              </button>
+              <div
+                class="modal fade"
+                id="ubah-admin"
+                tabindex="-1"
+                aria-labelledby="ubah-admin"
+                aria-hidden="true"
+              >
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="ubah-admin">
+                        Ubah Data Admin
+                      </h5>
+                      <button
+                        type="button"
+                        class="close"
+                        data-dismiss="modal"
+                        aria-label="Close"
+                      >
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      <form>
+                        <div class="form-group">
+                          <label
+                            for="ubah-nama-admin"
+                            class="col-form-label"
+                            >Nama</label
+                          >
+                          <input
+                            type="text"
+                            class="form-control"
+                            id="ubah-nama-admin"
+                          />
+                        </div>
+                        <div class="form-group">
+                          <label
+                            for="ubah-username-admin"
+                            class="col-form-label"
+                            >Username</label
+                          >
+                          <input
+                            type="text"
+                            class="form-control"
+                            id="ubah-username-admin"
+                          />
+                        </div>
+                      </form>
+                    </div>
+                    <div class="modal-footer">
+                      <button
+                        type="button"
+                        class="btn btn-secondary"
+                        data-dismiss="modal"
+                      >
+                        Close
+                      </button>
+                      <button type="button" class="btn btn-primary">
+                        Ubah
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <button type="button" class="btn btn-danger">
+                <i class="far fa-trash-alt"></i>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>Programming</td>
+            <td>Programming Blug</td>
+            <td class="text-success">Aktif</td>
+            <td>
+              <button type="button" class="btn btn-info">
+                <i class="fas fa-lock"></i>
+              </button>
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-toggle="modal"
+                data-target="#ubah-admin"
+              >
+                <i class="fas fa-pen"></i>
+              </button>
+              <button type="button" class="btn btn-danger">
+                <i class="far fa-trash-alt"></i>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+@endsection
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,53 @@
+<!-- Sidebar -->
+<ul
+class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+id="accordionSidebar"
+>
+<!-- Sidebar - Brand -->
+<a
+  class="sidebar-brand d-flex align-items-center justify-content-center"
+  href="index.html"
+>
+  <div class="sidebar-brand-icon">
+    <img src="logo-blug.png" alt="" srcset="" />
+  </div>
+  <div class="sidebar-brand-text mr-3">BLUG</div>
+</a>
+
+<!-- Divider -->
+<hr class="sidebar-divider my-0" />
+
+<!-- Nav Item - Dashboard -->
+<li class="nav-item active">
+  <a class="nav-link" href="index.html">
+    <i class="fas fa-users"></i>
+    <span>Admin</span></a
+  >
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider" />
+
+<!-- Heading -->
+<div class="sidebar-heading">Content</div>
+
+<!-- Nav Item - Pages Collapse Menu -->
+<li class="nav-item">
+  <a class="nav-link" href="#">
+    <i class="fas fa-photo-video"></i>
+    <span>Content</span>
+  </a>
+</li>
+
+<!-- Nav Item - Utilities Collapse Menu -->
+<li class="nav-item">
+  <a class="nav-link collapsed" href="#">
+    <i class="fas fa-photo-video"></i>
+    <span>Content</span>
+  </a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider" />
+</ul>
+<!-- End of Sidebar -->
