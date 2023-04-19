0c67fd1e7a30b4a953abc9ed10cc40782ec64f69
charlesbintang <charlesbintang203@gmail.com>
2023-04-20 02:34:59 +0700

    admin models

--- app/Models/admin.php
+++ app/Models/admin.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -11,4 +13,49 @@ use Laravel\Sanctum\HasApiTokens;
 class Admin extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    protected $table = 'admins';
+
+    protected $fillable = [
+        'name',
+        'username',
+        'password',
+        'status'
+    ];
+
+    /**
+     * Kita override boot method
+     *
+     * Mengisi primary key secara otomatis dengan UUID ketika membuat record
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = Str::uuid()->toString();
+            }
+        });
+    }
+
+    /**
+     * Kita override getIncrementing method
+     *
+     * Menonaktifkan auto increment
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * Kita override getKeyType method
+     *
+     * Memberi tahu laravel bahwa model ini menggunakan primary key bertipe string
+     */
+    public function getKeyType()
+    {
+        return 'string';
+    }
 }
+
+?>
